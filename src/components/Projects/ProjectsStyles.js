4b4036b137d6daa3027ae24273719f472c149452
ncrawford22 <nathanwcrawford22@gmail.com>
2022-03-02 17:42:35 -0500

    Update ProjectsStyles.js

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -27,7 +27,7 @@ export const BlogCard = styled.div`
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
   object-fit: contain;
-  height: 285px;
+  height: 350px;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
